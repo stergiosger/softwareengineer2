6d59506c5ffbbe8d5f46a01cac4ff213ce681e93
stergiosger <72515736+stergiosger@users.noreply.github.com>
2022-12-16 03:44:32 +0200

    Final re

--- src/incometaxcalculator/data/io/XMLLogWriter.java
+++ src/incometaxcalculator/data/io/XMLLogWriter.java
@@ -4,7 +4,7 @@ public class XMLLogWriter extends LogWriter {
 
   @Override
   protected String giveLogInfo(int i) {
-    String[] s = new String[]{"_LOG.xml","<Name> ","<AFM> ","<Income> ","<Basic Tax> ","<Tax Increase> ", "<Tax Decrease> ","<Total Tax> ","<TotalReceiptsGathered> ","<Entertainment:> ","<Basic> ","<Travel> ","<Health> ","<Other> ","</Name> ","</AFM> ","</Income> ","</Basic Tax> ","</Tax Increase> ", "</Tax Decrease> ","</Total Tax> ","</TotalReceiptsGathered> ","</Entertainment:> ","</Basic> ","</Travel> ","</Health> ","</Other> "};
+    String[] s = new String[]{"_LOG.xml","<Name> ","<AFM> ","<Income> ","<Basic Tax> ","<Tax Increase> ", "<Tax Decrease> ","<Total Tax> ","<TotalReceiptsGathered> ","<Entertainment> ","<Basic> ","<Travel> ","<Health> ","<Other> ","</Name> ","</AFM> ","</Income> ","</Basic Tax> ","</Tax Increase> ", "</Tax Decrease> ","</Total Tax> ","</TotalReceiptsGathered> ","</Entertainment> ","</Basic> ","</Travel> ","</Health> ","</Other> "};
     return  s[i];
   };
 

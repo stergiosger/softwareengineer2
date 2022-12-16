9b8237d2c71aa61a235e38395c450e7faa50374f
stergiosger <72515736+stergiosger@users.noreply.github.com>
2022-12-16 03:43:50 +0200

    XMLLogWriter. Final

---
+++ XMLLogWriter.java
@@ -0,0 +1,11 @@
+package incometaxcalculator.data.io;
+
+public class XMLLogWriter extends LogWriter {
+
+  @Override
+  protected String giveLogInfo(int i) {
+    String[] s = new String[]{"_LOG.xml","<Name> ","<AFM> ","<Income> ","<Basic Tax> ","<Tax Increase> ", "<Tax Decrease> ","<Total Tax> ","<TotalReceiptsGathered> ","<Entertainment> ","<Basic> ","<Travel> ","<Health> ","<Other> ","</Name> ","</AFM> ","</Income> ","</Basic Tax> ","</Tax Increase> ", "</Tax Decrease> ","</Total Tax> ","</TotalReceiptsGathered> ","</Entertainment> ","</Basic> ","</Travel> ","</Health> ","</Other> "};
+    return  s[i];
+  };
+
+}
